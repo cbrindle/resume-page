18fc2114f10783b7750f16172e0a1a2c6486e46b
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-02-01 14:50:16 -0500

    Loading Spinner Created a page loading spinner component to display while content is lazy-loading for a better user experience.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,3 +2,5 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import MainRouter from './MainRouter';
 import Context from './components/Context/Context';
+
+import Spinner from './components/Spinner/Spinner';
