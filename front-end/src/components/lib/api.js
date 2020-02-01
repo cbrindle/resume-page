703d7324cca35cf02409362c712a51664c40e9fb
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-02-01 14:41:33 -0500

    Framework Created frame work for front and back end. Needs testing to make sure basic functionality of spinning up the page works.

---
+++ front-end/src/components/lib/api.js
@@ -0,0 +1,26 @@
+import Axios from './Axios/Axios';
+import jwt_decode from 'jwt-decode';
+import { createBrowserHistory } from 'history';
+import setAuthToken from '../lib/Axios/setAuthToken';
+
+export const checkTokenAuth = () => {
+
+    let jwtToken = localStorage.getItem('jwtToken-reddit');
+
+    if (jwtToken) {
+
+        let decoded = jwt_decode(jwtToken);
+        const currentTime = Date.now() / 1000;
+        if (decoded.exp < currentTime) {
+            localStorage.removeItem('jwtToken-reddit');
+
+            // TODO: Redirect somewhere after token is removed
+            // createBrowserHistory().push('/sign-in')
+            
+            setAuthToken(null)
+        } else {
+            setAuthToken(jwtToken)
+            return decoded;
+        }
+    }
+}
