69db3043b67de3ab8d78fb2c7048caec2c9a5f48
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-02-04 12:41:58 -0500

    Removed unneeded code from App.js file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Context from './components/Context/Context';
 import MainRouter from './MainRouter'
 
