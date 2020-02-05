0348a122ae5723cc93ef3f8dbbb813a5ec4846ff
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-02-05 16:26:18 -0500

    Added Rugby hobby image and content to Misc component. Need to created Rugby modal.

--- front-end/src/components/Misc/Misc.js
+++ front-end/src/components/Misc/Misc.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import brindleMisc from '../../images/brindle-misc.png';
-
+import hobbyRugby from '../../images/hobbyRugby.jpg';
 
 export default class Misc extends Component {
     render() {
         return (
             <div style={styles.main}>
-                <div style={styles.center}>
+                <div style={styles.centerTitle}>
                     <h1 style={styles.title}>Miscellaneous</h1>
                 </div>
-
+                <div style={styles.centerContent}>
+                    <div>
+                        <p style={{textAlign:'center'}}>--- Hobbies ---</p>
+                        <img src={hobbyRugby} alt="Brindle playing rugby, putting a step on a fool." style={styles.linkImage} />
+                    </div>
+                </div>
                 <div style={styles.imgArea}>
                     <img src={brindleMisc} style={styles.rightImg} alt="Bitmoji of Brindle at parachuting onto the web page" />
                 </div>
@@ -24,16 +29,25 @@ const styles = {
         height: '100vh',
         width: '85vw',
         gridTemplateColumns: '10% 60% 30%',
-        gridTemplateRows: '10% 85% 5%'
+        gridTemplateRows: '10% 15% 70% 5%'
     },
-    center: {
+    centerTitle: {
         display: 'flex',
         flexDirection: 'column',
         gridArea: '2 / 2 / span 1 / span 1',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     title: {
         fontSize: '3em'
+    },
+    centerContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        gridArea: '3 / 2 / span 1 / span 1',
+        fontSize: '1.2em',
+        alignItems: 'center',
+        overflowY: 'scroll'
     },
     imgArea: {
         display: 'flex',
@@ -44,5 +58,9 @@ const styles = {
     rightImg: {
         width: '75%',
         height: '40%'
+    },
+    linkImage: {
+        borderRadius: '3px',
+        cursor: 'pointer'
     }
 }
