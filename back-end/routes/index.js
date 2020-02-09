db24f78debaaea552916f27f46de63070e698631
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-02-09 14:46:30 -0500

    Pushing to publish test

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/awake', function(req, res) {
+  res.json({message: ''})
+})
+
 module.exports = router;
