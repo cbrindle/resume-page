c0df7d0f44a5cbfcd43a8548beccfda93ba7b3fc
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-02-15 13:53:56 -0500

    Testing mobile viewing

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -7,21 +7,21 @@ import Spinner from './components/Spinner/Spinner';
 const Home = React.lazy(() => import('./components/Home/Home'));
 const Education = React.lazy(() => import('./components/Education/Education'));
 const WorkExp = React.lazy(() => import('./components/WorkExp/WorkExp'));
-const Coding = React.lazy(() => import('./components/Coding/Coding'));
 const Projects = React.lazy(() => import('./components/Projects/Projects'));
 const Contact = React.lazy(() => import('./components/Contact/Contact'));
 const Misc = React.lazy(() => import('./components/Misc/Misc'));
 const CodingAlt = React.lazy(() => import('./components/Coding/CodingAlt'));
+const HomeMobile = React.lazy(() => import('./components/Home/HomeMobile'));
 
 export default class MainRouter extends Component {
     render() {
         return (
             <>
                 <div style={styles.main}>
-                    <Nav />
+                    {typeof window.orientation !== "undefined" ? '' : <Nav />}
                     <React.Suspense fallback={<Spinner />}>
                         <Switch>
-                            <Route exact path="/" component={Home} />
+                            {typeof window.orientation !== "undefined" ? <Route exact path="/" component={HomeMobile} /> : <Route exact path="/" component={Home} />}
                             <Route exact path="/education" component={Education} />
                             <Route exact path="/work-experience" component={WorkExp} />
                             <Route exact path="/coding" component={CodingAlt} />
---
+++ src/components/Home/HomeMobile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HomeMobile extends Component {
+    render() {
+        return (
+            <div>
+                HOME
+            </div>
+        )
+    }
+}
